2aec5ef33f13a3391c24638ba3a89bb84b707db7
Mostapha Rammo <mr.rammo@gmail.com>
2024-03-16 14:58:01 -0400

    Second Ex

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::fs;
 use clap::{Parser, Subcommand};
+use anyhow::Context;
+use std::ffi::CStr;
+use std::io::prelude::*;
+use std::io::{BufReader};
+use flate2::read::ZlibDecoder;
+
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -11,13 +17,23 @@ struct Args {
 #[derive(Debug, Subcommand)]
 enum Command {
     Init,
+    CatFile {
+        #[clap(short = 'p')]
+        pretty_print: bool,
+
+        object_hash: String,
+    }
 }
 
-fn main() {
+enum Kind {
+    Blob,
+}
+
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    eprintln!("Logs from your program will appear here!");
 
     match args.command {
         Command::Init => {
@@ -26,6 +42,63 @@ fn main() {
             fs::create_dir(".git/refs").unwrap();
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
             println!("Initialized git directory")
+        },
+        Command::CatFile { pretty_print, object_hash } => {
+
+            anyhow::ensure!(
+                pretty_print,
+                "mode must be given without -p and we don't support mode"
+            );
+
+            // TODO: support shortest unique object hashes
+            let f = std::fs::File::open(format!(
+                ".git/objects/{}/{}",
+                &object_hash[..2],
+                &object_hash[2..]
+            ))
+            .context("open in .git/objects")?;
+
+            let z = ZlibDecoder::new(f);
+            let mut z = BufReader::new(z);
+            let mut buf = Vec::new();
+            z.read_until(0, &mut buf)
+                .context("read header from .git/objects")?;
+            let header = CStr::from_bytes_with_nul(&buf)
+                .expect("know there is exactly one nul, and it's at the end");
+            let header = header
+                .to_str()
+                .context(".git/objects file header isn't valid UTF-8")?;
+            let Some((kind, size)) = header.split_once(' ') else {
+                anyhow::bail!(".git/objects file header did not start with a known type: '{header}'")
+            };
+
+            let kind = match kind {
+                "blob" => Kind::Blob,
+                _ => anyhow::bail!("Unknown Kind: '{kind}'")
+            };
+
+            let size = size
+                .parse::<usize>()
+                .context(".git/objects file header has invalid size: {size}")?;
+            buf.clear();
+            buf.resize(size, 0);
+            z.read_exact(&mut buf)
+                .context(".git/objects file size is not what's expected")?;
+            let n = z
+                .read(&mut [0])
+                .context("validate EOF in .git/objects file")?;
+            anyhow::ensure!(n == 0, ".git/object fileh had {n} trailing bytes");
+
+            let stdout = std::io::stdout();
+            let mut stdout = stdout.lock();
+
+            match kind {
+                Kind::Blob => stdout
+                    .write_all(&buf)
+                    .context("write object contents to stdout")?,
+            }
         }
     }
+
+    Ok(())
 }
